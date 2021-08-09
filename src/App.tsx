33898c328b3341c872b8bf3dec343c25e5d5affe
nedashkivskyios <nedashkivskyios@gmail.com>
2021-08-09 17:45:54 +0300

    todolist 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { v1 } from 'uuid';
 import './App.css';
-import ToDoList from "./ToDoList";
+import ToDoList, {TasksPropsType} from "./ToDoList";
+
+export type FilterType = 'all' | 'done' | 'notdone'
 
 function App() {
+  let [tasks, setTasks] = useState([
+    {id: v1(), title: "HTML&CSS", isDone: true},
+    {id: v1(), title: "JS", isDone: true},
+    {id: v1(), title: "React", isDone: false},
+    {id: v1(), title: "rest api", isDone: false},
+    {id: v1(), title: "graphQL", isDone: false}
+  ])
 
-  const firstTask = [
-    {id: 0, title: "HTML&CSS", isDone: true},
-    {id: 1, title: "JS", isDone: true},
-    {id: 2, title: "React", isDone: false}
-  ]
+  const removeTask = (taskId: string) => {
+    const removedTasks = tasks.filter(el => el.id !== taskId)
+    setTasks(removedTasks)
+  }
+  let filteringTasks = tasks
+  let [filter, setFilter] = useState<FilterType>('all')
 
-  const secondTask = [
-    {id: 0, title: "ReactJS", isDone: true},
-    {id: 1, title: "Vue", isDone: false},
-    {id: 2, title: "Angular", isDone: false}
-  ]
+  const addTask = (title: string) => {
+   let  newTask = {id: v1(), title: title, isDone: false}
+    let taskForToDoList = [...tasks, newTask]
+    setTasks(taskForToDoList)
+  }
 
+  if (filter === 'done') {
+    filteringTasks = filteringTasks.filter(el => el.isDone)
+  }
+  if (filter === 'notdone') {
+    filteringTasks = filteringTasks.filter(el => !el.isDone)
+  }
 
   return (
     <div className="App">
-      <ToDoList title={"How to learn"} task={firstTask}/>
-      <ToDoList title={"Frameworks"} task={secondTask}/>
+      <ToDoList addTask={addTask} title={"How to learn"} task={filteringTasks} removeTask={removeTask} setFilter={setFilter}/>
     </div>
   );
 }
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,32 +1,62 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
+import {FilterType} from "./App";
 
-type TasksPropsType = {
-  id: number,
+export type TasksPropsType = {
+  id: string,
   title: string,
   isDone: boolean
 }
-
-type ToDoListPropsType = {
+export type ToDoListPropsType = {
   title: string
   task: Array<TasksPropsType>
+  removeTask: (taskId: string) => void
+  setFilter: (setFilter: FilterType) => void
+  addTask: (title: string) => void
 }
 
-
 const ToDoList = (props: ToDoListPropsType) => {
+
+  const [todoTitle, setTodoTitle] = useState<string>('')
+
+  const todoTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodoTitle(e.currentTarget.value)
+  }
+  const addTaskButtonHandler = () => {
+    props.addTask(todoTitle)
+    setTodoTitle('')
+  }
+  const allButtonHandler = () => {
+    props.setFilter('all')
+  }
+  const activeButtonHandler = () => {
+    props.setFilter('notdone')
+  }
+  const completedButtonHandler = () => {
+    props.setFilter('done')
+  }
+
   return (
     <div className="ToDoList">
       <h3>{props.title}</h3>
       <div>
-        <input/>
-        <button>+</button>
+        <input value={todoTitle} onChange={todoTitleHandler}/>
+        <button onClick={addTaskButtonHandler}>+</button>
       </div>
       <ul>
-        {props.task.map(el => <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>)}
+        {props.task.map(el =>
+          <li key={el.id}>
+            <button onClick={() => props.removeTask(el.id)}>
+              x
+            </button>
+            <input type="checkbox" checked={el.isDone}/>
+            <span>{el.title}</span>
+          </li>
+        )}
       </ul>
       <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button onClick={allButtonHandler}>All</button>
+        <button onClick={activeButtonHandler}>Active</button>
+        <button onClick={completedButtonHandler}>Completed</button>
       </div>
     </div>
   )
